95f165f7fd472910bf5a81ab91d5edc12dadaa44
sajeeb-ahmeed <sajeebahmed1971.bd@gmail.com>
2022-01-01 10:52:27 +0600

    jumpiing

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
 
        // alert('Welcome You');
-        document.write("  Name:<i> SaJeeb Ahmed </i>  <br/>");
-        document.write(" Address:  Narsingdi , Dhaka , Bangladesh <br/> ");
-        document.write("Skils: HTML-5 , CSS3 , BOOTSTRAP-5 , JS BASIC <br/> ");
-       document.write("Phone: 01748402018  <br/>");
-       // typeof("sajeebahmed")
-
-
-var age = 5 ;
-var goal = " Work to make web developer"
-
-
-console.log(typeof (age));
+      
