67b3e8b16a8afc58880f8ad4040c970e9ef2e8fa
sajeeb-ahmeed <sajeebahmed1971.bd@gmail.com>
2022-01-01 22:01:17 +0600

    commit added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+// alert('Welcome You');
 
-       // alert('Welcome You');
-      
+alert("Welcome To You");
+var fristName = prompt("Enter Frist Name : ");
+var lastName = prompt(" Last Name : ");
+var fullname = (fristName.concat(lastName));
+document.write(fullname + "  -- number of charecter  " + fullname.length);
